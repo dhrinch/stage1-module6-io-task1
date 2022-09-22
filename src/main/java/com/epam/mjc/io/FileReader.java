54ddc3aa1976561737e55af4eb4d5dbbea71820e
dhrinch <dhrinch@gmail.com>
2022-09-22 15:41:35 +0200

    Solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Profile profile = new Profile();
+
+        try {
+            FileInputStream is = new FileInputStream(file);
+            byte[] buffer = new byte[is.available()];
+            while (is.read(buffer) > 0) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte chr : buffer) {
+                    stringBuilder.append((char)chr);
+                }
+                String result = stringBuilder.toString();
+                String[] data = result.split("\n");
+
+                profile.setName(data[0].split(":")[1].trim());
+                profile.setAge(Integer.parseInt(data[1].split(":")[1].trim()));
+                profile.setEmail(data[2].split(":")[1].trim());
+                profile.setPhone(Long.parseLong(data[3].split(":")[1].trim()));
+            }
+            is.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
